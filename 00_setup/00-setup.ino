c6bc461b2bc05a9f4025bb486a9c919d498073fb
Josef Polasek <josefpolasek12@gmail.com>
2023-09-06 19:40:51 +0300

    setup code updated

--- 00_setup/00-setup.ino
+++ 00_setup/00-setup.ino
@@ -1,12 +1,16 @@
+#include <Arduino.h>
+#include <Wire.h>
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT); // initialize digital pin LED_BUILTIN as an output.
+  Serial.begin(9600);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(100);                      // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-  delay(100);                      // wait for a second
+  digitalWrite(LED_BUILTIN, HIGH);  
+  delay(1000);                      
+  digitalWrite(LED_BUILTIN, LOW);  
+  delay(1000);                     
+  Serial.println("one loop"); // open the serial monitor to see this message
 }
